98f076dc083fcdd6995e8232ac9007e1b33946d1
TheAsouka <32157187+TheAsouka@users.noreply.github.com>
2018-06-02 02:42:54 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
 function setup() {
   //createCanvas(710, 500);
-  createCanvas(200, 200, WEBGL);
+  var cnv = createCanvas(200, 200, WEBGL);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  y = y + 100;
+  cnv.position(x, y);
+
 }
 
 function rand(min,max){
-	return Math.floor(Math.random() * max) + min;
+    return Math.floor(Math.random() * max) + min;
 }
 
 var count = -1;
 var speed = 0.01;
-var isRotate = "";
+var isRotate = "Off";
+var p1 = rand(0,255);
+var p2 = rand(0,255);
+var p3 = rand(0,255);
 
 function startRotation(){
   //console.log("WORK");
@@ -41,16 +49,14 @@ function countUP(){
   return count;
 }
 
-var p1 = rand(0,255);
-var p2 = rand(0,255);
-var p3 = rand(0,255);
+function changeBG(){
+  p1 = rand(0,255);
+  p2 = rand(0,255);
+  p3 = rand(0,255);
+}
 
 function draw(){
   background(p1,p2,p3);
   startRotation();
   box(50);
-}
-
-
-
-
+}  
